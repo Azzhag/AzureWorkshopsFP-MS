73819cd711bec12f018e6092272c383b338af4ad
Krzysztof Szabelski <kszabelski@gmail.com>
2016-02-22 23:01:43 +0100

    Template for FilesStorageService implementation

--- Common/FilesStorageService.cs
+++ Common/FilesStorageService.cs
@@ -7,6 +7,15 @@ namespace Common
     {
         public Task<string> UploadFile(string fileName, Stream file, string mimeType)
         {
+            ////var storageAccount = CloudStorageAccount.Parse("azureStorageConnectionString");
+            ////var blobClient = storageAccount.CreateCloudBlobClient();
+            ////var container = blobClient.GetContainerReference("containername");
+            ////container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Container });
+            ////container.CreateIfNotExists();
+            ////var blockBlob = container.GetBlockBlobReference(fileName);
+            ////blockBlob.Properties.ContentType = mimeType;
+            ////await blockBlob.UploadFromStreamAsync(file);
+
             return Task.FromResult(string.Empty);
         }
     }
